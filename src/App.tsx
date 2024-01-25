4b5578b98040f5bb7db8c580e97124b105925c16
Elizaveta Spodynska <elizavetasp@wizardsdev.com>
2024-01-26 00:19:29 +0100

    commit 26.01

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Info from './pages/Info';
-import User from './pages/User';
-import Tours from './pages/Tours';
+import User from './pages/User/User';
+import Tours from './pages/Tours/Tours';
 import Login from './pages/Login/Login';
 import Profile from './pages/Profile';
 import Tour from './pages/Tour';
-import AddTour from './pages/AddTour';
+import AddTour from './pages/AddTour/AddTour';
 import Registration from './pages/Registration/Registration';
 
 //ant-design
--- src/pages/Tours.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Tours() {
-  return <>Tours Page</>;
-}
-
-export default Tours;
